e94d56a2395903cc3712785d603959c415959193
RafiSuryadani <rafisuryadani007@gmail.com>
2024-02-03 10:41:05 +0700

    Done

--- js/bmi-calc.js
+++ js/bmi-calc.js
@@ -6,5 +6,21 @@ function bmiCalc(){
 
     bmiCalcCount = (weight / Math.pow((height/100),2)).toFixed(2)
     document.getElementById('resultBmi').textContent = bmiCalcCount
+
+    if(bmiCalcCount <18.5){
+        indicator = "Underweight"
+    }else if(bmiCalcCount >= 18.5 && bmiCalcCount <= 24.9){
+        indicator = "Nomral Weight"
+    }else if(bmiCalcCount >= 25.0 && bmiCalcCount <= 29.9){
+        indicator = "Overweight"
+    }else if(bmiCalcCount >=30.0 && bmiCalcCount <= 34.9){
+        indicator = "Obese"
+    }else{
+        indicator = "Extremelyobese"
+    }
+
+    console.log('test', indicator)
+    document.getElementById('resultIndicator').textContent = indicator
+    
     
 }
